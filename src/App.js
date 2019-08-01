5891351f41a6f1c67be86661631aa3b31618a73d
udonka <icymasa@gmail.com>
2019-08-01 17:30:39 +0900

    css を sass にして pug 導入しようとしている

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.sass';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    pug`
+      .c-page-wrapper
+        .e-header
+          .c-header
+            h1 fumi.ninja 
+        .e-content aiueo
+        .e-footer
+          .c-controls
+            p ninja 
+    `
   );
 }
 
